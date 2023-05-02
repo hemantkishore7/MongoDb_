b856553727be08505ee59a2a852c6f1ec5563131
Hemant Kishore <109723456+hemantkishore7@users.noreply.github.com>
2023-05-03 00:24:04 +0530

    Update

--- day-2/Queries.js
+++ day-2/Queries.js
@@ -46,3 +46,92 @@ db.codekata.aggregate([{
     userid:1
   }
 }])
+
+
+//Q3:Find all the topics and tasks which are thought in the month of October
+
+db.topics.aggregate([{
+    $lookup: {
+        from: "tasks",
+        localField: "topicid",
+        foreignField: "topicid",
+        as: "taskinfo",
+    },
+},
+{
+    $match: {
+        $and: [{
+                $or: [
+                    { topic_date: { $gt: new Date("30-sep-2020") } },
+                    { topic_date: { $lt: new Date("1-nov-2020") } },
+                ],
+            },
+
+            {
+                $or: [
+                    { "taskinfo.due_date": { $gt: new Date("30-sep-2020") } },
+                    { "taskinfo.due_date": { $lt: new Date("1-nov-2020") } },
+                ],
+            },
+        ],
+    },
+},
+]);
+
+//Q5:Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020
+
+db.attendance.aggregate([{
+    $lookup: {
+        from: "topics",
+        localField: "topicid",
+        foreignField: "topicid",
+        as: "topics",
+    },
+},
+{
+    $lookup: {
+        from: "tasks",
+        localField: "topicid",
+        foreignField: "topicid",
+        as: "tasks",
+    },
+},
+{ $match: { $and: [{ attended: false }, { "tasks.submitted": false }] } },
+{
+    $match: {
+        $and: [{
+                $or: [
+                    { "topics.topic_date": { $gte: new Date("15-oct-2020") } },
+                    { "topics.topic_date": { $lte: new Date("31-oct-2020") } },
+                ],
+            },
+            {
+                $or: [
+                    { "tasks.due_date": { $gte: new Date("15-oct-2020") } },
+                    { "tasks.due_date": { $lte: new Date("31-oct-2020") } },
+                ],
+            },
+        ],
+    },
+},
+{
+    $count: "No_of_students_absent",
+},
+]);
+
+//Q1:Find all the mentors with who has the mentee's count more than 15
+
+db.batchInfo.aggregate([{$match:{strength:{$gte:15}}},
+{$lookup:{
+  from:'mentors',
+  localField:'mentorid',
+  foreignField:'mentorid',
+  as:'mentorInfo'
+}},
+{$project:{
+  _id:0,
+  'mentorInfo.mentorid':1,
+  'mentorInfo.mentorname':1,
+  strength:1
+}}
+])
